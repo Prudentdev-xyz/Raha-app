8a7d79fd657a28393ed8827ff9a183470fc7e25f
AbdQoharr Olayiwola <olayiwolaabdqoharr226!gmail.com>
2025-11-13 09:18:50 -0800

    fix-AboutSection1

---
+++ Raha-app/src/Components/layouts/About.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+
+export const About = () => {
+  return <div className="p-20 md:p-40 bg-primary">
+    <h1 className="text-tetiary font-bold text-4xl md:text-8xl flex items-center justify-center text-center">About Us</h1>
+    <p className="text-tetiary flex items-center justify-center text-center pt-1 text-sm md:text-xl">Home / About</p>
+    <div>
+      <img src="" alt="" />
+    </div>
+  </div>;
+};
--- Raha-app/src/pages/AboutUs.jsx
+++ Raha-app/src/pages/AboutUs.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Header } from '../components/layouts/Header'
 import { Footer } from '../components/layouts/Footer'
 import { HeroAboutUs } from '../components/layouts/HeroAboutUs'
+import { About } from '../components/layouts/About'
 
 export const AboutUs = () => {
   return (
     <div>
         <div>
+          <About />
           <HeroAboutUs />
         </div>
     </div>
